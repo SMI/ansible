83669fcf1e69bfd21c1e2f8307dae7da3a984410
Ruairidh MacLeod <ruairidh@rkm.dev>
2023-11-21 17:04:47 +0000

    update build script to run playbook

--- build.bash
+++ build.bash
@@ -6,19 +6,15 @@ IMAGE="smi/aio"
 CONTAINER_NAME="${IMAGE/\//_}_build"
 
 docker build --pull --tag "${IMAGE}:build" .
-
-docker kill "${CONTAINER_NAME}" || true
+docker container rm -f "${CONTAINER_NAME}" || true
 docker run --rm -d --name "${CONTAINER_NAME}" "${IMAGE}:build"
 
-ansible \
+ansible-playbook \
     --connection docker \
     --inventory "${CONTAINER_NAME}," \
-    --extra-vars \
-        ansible_python_interpreter=python3.10 \
-    -m ping \
-    all
-
-# run ansible
+    -e "ansible_python_interpreter=python3.10" \
+    -e "host_role=Docker AIO" \
+    site.yml
 
 docker commit "${CONTAINER_NAME}" "${IMAGE}:latest"
 docker kill "${CONTAINER_NAME}"
