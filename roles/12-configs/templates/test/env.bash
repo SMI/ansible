7f49893fb3fd75277f16e82a1659f9afbc9292f4
Ruairidh MacLeod <ruairidh@rkm.dev>
2023-11-29 16:44:24 +0000

    refactor role variables and inventory

--- roles/12-configs/templates/test/env.bash
+++ roles/12-configs/templates/test/env.bash
@@ -1,4 +1,4 @@
 # {{ ansible_managed }}
 
 export SMI_SMISERVICES_VERSION="5.4.0"
-export SMI_LOGS_ROOT="{{ smi_service_software_logs_dir }}/{{ config_name }}"
+export SMI_LOGS_ROOT="{{ env_logs_dir }}"
