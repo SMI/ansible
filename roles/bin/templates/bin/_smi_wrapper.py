590de2eac1eb00e1a0d47bb6119d4ce33d1cf681
Ruairidh MacLeod <ruairidh@rkm.dev>
2024-09-16 14:34:36 +0100

    ensure env is loaded when wrapper called

--- roles/bin/templates/bin/_smi_wrapper.py
+++ roles/bin/templates/bin/_smi_wrapper.py
@@ -5,18 +5,24 @@ import subprocess
 import sys
 
 
-def _env_from(file_path: str) -> dict[str, str]:
-    env = {}
-    with open(file_path) as f:
-        for line in f.read().splitlines():
-            if not line.startswith("export"):
-                continue
-            var, val = line.split(" ", 1)[1].split("=")
-            env[var] = val.strip('"')
-    return env
+assert "SMI_ROOT" in os.environ, "SMI_ROOT must be set"
+smi_root = os.environ["SMI_ROOT"]
+
+assert "SMI_ENV" in os.environ, "SMI_ENV must be set"
+smi_env = os.environ["SMI_ENV"]
+
+env_dir = f"{smi_root}/envs/{smi_env}"
+assert os.path.isdir(env_dir), f"Expected {env_dir} to be a directory"
+
+with open(f"{env_dir}/env.bash") as f:
+    for line in f.read().splitlines():
+        if not line.startswith("export"):
+            continue
+        var, val = line.split(" ", 1)[1].split("=")
+        os.environ[var] = val.strip('"')
 
 
-def init() -> tuple[argparse.Namespace, list[str], dict[str, str], str]:
+def init() -> tuple[argparse.Namespace, list[str]]:
     parser = argparse.ArgumentParser()
     wrapper_group = parser.add_argument_group("Wrapper args")
     wrapper_group.add_argument("-v", "--verbose", action="store_true")
@@ -39,32 +45,19 @@ def init() -> tuple[argparse.Namespace, list[str], dict[str, str], str]:
     if wrapper_args.copies > 1:
         wrapper_args.detach = True
 
-    assert "SMI_ROOT" in os.environ, "SMI_ROOT must be set"
-    smi_root = os.environ["SMI_ROOT"]
-
-    assert "SMI_ENV" in os.environ, "SMI_ENV must be set"
-    smi_env = os.environ["SMI_ENV"]
-
-    env_dir = f"{smi_root}/envs/{smi_env}"
-    assert os.path.isdir(env_dir), f"{env_dir} does not exist"
-    env = {**os.environ, **_env_from(f"{env_dir}/env.bash")}
-
-    if wrapper_args.verbose:
-        for var in sorted(x for x in env if x.startswith("SMI_")):
-            print(f"{var}={env[var]}")
-
-    return (wrapper_args, remaining_argv, env, env_dir)
+    return (wrapper_args, remaining_argv)
 
 
 def run(
     wrapper_args: argparse.Namespace,
     remaining_argv: list[str],
     cmd: tuple[str, ...],
-    env: dict[str, str],
 ) -> None:
     cmd = (*cmd, *remaining_argv)
 
     if wrapper_args.verbose:
+        for var in sorted(x for x in os.environ if x.startswith("SMI_")):
+            print(f"{var}={os.environ[var]}")
         if wrapper_args.copies > 1:
             print(f"Executing {wrapper_args.copies} detached instances of:")
         subprocess.check_call(("echo", "$", *cmd))
@@ -73,23 +66,19 @@ def run(
         for _ in range(wrapper_args.copies):
             subprocess.Popen(
                 cmd,
-                env=env,
                 stdin=subprocess.DEVNULL,
                 stdout=subprocess.DEVNULL,
                 stderr=subprocess.DEVNULL,
             )
     else:
         try:
-            subprocess.check_call(
-                cmd,
-                env=env,
-            )
+            subprocess.check_call(cmd)
         except KeyboardInterrupt:
             pass
 
 
 def run_smiservices(config_path: str, single_instance: bool = False) -> None:
-    wrapper_args, remaining_argv, env, config_dir = init()
+    wrapper_args, remaining_argv = init()
 
     if (wrapper_args.copies > 1 or wrapper_args.detach) and single_instance:
         raise ValueError(
@@ -99,4 +88,4 @@ def run_smiservices(config_path: str, single_instance: bool = False) -> None:
     smi_bin = os.environ["SMI_SMISERVICES_BIN"]
     app_name = sys.argv[0].rpartition("smi-")[-1]
     cmd = (smi_bin, app_name, "-y", config_path)
-    run(wrapper_args, remaining_argv, cmd, env)
+    run(wrapper_args, remaining_argv, cmd)
