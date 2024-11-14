2a5ff089fa921765f7d43a74bb3fbe7055d4342e
Ruairidh MacLeod <ruairidh@rkm.dev>
2024-11-14 13:29:30 +0000

    add smi-update-values wrapper and new env var

--- roles/envs/templates/example/env.bash
+++ roles/envs/templates/example/env.bash
@@ -3,5 +3,6 @@
 
 export SMI_SMISERVICES_VERSION="v5.6.1"
 export SMI_SMISERVICES_BIN="{{ envs_smi_root_dir }}/software/SMI/SmiServices/${SMI_SMISERVICES_VERSION}/smi/smi"
+export SMI_SMISERVICES_INGEST_PIPELINE_CONFIG="smi-services-ingest-pipeline.yaml"
 export SMI_SMISERVICES_EXTRACT_PIPELINE_CONFIG="smi-services-extract-pipeline.yaml"
 export SMI_SMISERVICES_CTP_JAR="{{ envs_smi_root_dir }}/software/SMI/SmiServices/${SMI_SMISERVICES_VERSION}/CTPAnonymiser.jar"
